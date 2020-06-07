e630f8b28270e1b1e3bbc27d159d0d0fff4f6020
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-07 12:39:45 +0530

    Sarthak/Girish : Adds __repr__ and __str__ to Position class

--- position.py
+++ position.py
@@ -25,3 +25,10 @@ class Position:
 
     def __eq__(self, other: Position) -> bool:
         return self.__value == other.__value
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        x_position, y_position = self.__value
+        return f'Position, x = {x_position} and y = {y_position}'
