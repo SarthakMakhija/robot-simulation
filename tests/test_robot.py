57ad9603f712612fa9129f295c80cec7bcb3b9eb
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-07 12:14:51 +0530

    Sarthak/Girish : Adds a test to move Robot up

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -17,6 +17,10 @@ class Position:
         x_position, y_position = self.__value
         return Position((x_position + 1, y_position))
 
+    def up(self) -> Position:
+        x_position, y_position = self.__value
+        return Position((x_position, y_position + 1))
+
     def __eq__(self, other: Position) -> bool:
         return self.__value == other.__value
 
@@ -35,6 +39,10 @@ class Robot:
 
     def move_right(self):
         self.__initial_position = self.__initial_position.right()
+        return self
+
+    def move_up(self):
+        self.__initial_position = self.__initial_position.up()
         return self
 
 
@@ -63,3 +71,11 @@ class RobotTest(TestCase):
         robot = Robot(initial_position=Position((10, 1)))
         robot.move_right()
         self.assertEqual(Position((11, 1)), robot.positioned_at())
+
+    def test_move_robot_up(self):
+        """
+        Moves the robot up, given it has been initialized with a position
+        """
+        robot = Robot(initial_position=Position((10, 1)))
+        robot.move_up()
+        self.assertEqual(Position((10, 2)), robot.positioned_at())
