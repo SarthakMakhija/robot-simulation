4956737bece0b1b99e4608cddb7dfd35b3fabaf7
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-07 14:09:08 +0530

    Sarthak/Girish : Adds a test in test_robot.py to ensure robot accepts movement commands

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -1,15 +1,24 @@
 from unittest import TestCase
 
 from position import Position
+from position_command import LeftPositionCommand
 from robot import Robot
 
 
 class RobotTest(TestCase):
 
-    def test_initialize_robot_with_a_sample_initial_position(self):
+    def test_position_of_robot(self):
         """
         Sets initial position for robot
         """
         robot = Robot(initial_position=Position((10, 1)))
         initial_position = robot.positioned_at()
         self.assertEqual(Position((10, 1)), initial_position)
+
+    def test_robot_accepts_commands_to_change_position(self):
+        """
+        Robot changes it position based on commands
+        """
+        robot = Robot(initial_position=Position((10, 1)))
+        robot.accept([LeftPositionCommand()])
+        self.assertEqual(Position((9, 1)), robot.positioned_at())
