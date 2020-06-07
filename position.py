dd9cbbc4cf8cdd8cb2b02a4e90f4d5702bd23513
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-07 14:19:55 +0530

    Sarthak/Girish : Hides the internal details of Position class

--- position.py
+++ position.py
@@ -6,18 +6,18 @@ from typing import Tuple, Callable
 class Position:
 
     def __init__(self, value: Tuple[int, int]):
-        self.value = value
+        self.__value = value
 
     def new_position(self, func: Callable[[int, int], Tuple[int, int]]) -> Position:
-        x_position, y_position = self.value
+        x_position, y_position = self.__value
         return Position(func(x_position, y_position))
 
     def __eq__(self, other: Position) -> bool:
-        return self.value == other.value
+        return self.__value == other.__value
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        x_position, y_position = self.value
+        x_position, y_position = self.__value
         return f'Position, x = {x_position} and y = {y_position}'
